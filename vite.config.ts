4c4c6e0006877ba26938fc894e7e654f51982c03
wiwitmikael-a11y <wiwitmikael@gmail.com>
2025-10-19 00:56:50 +0700

    feat: Update dependencies and remove unused config
    Downgrades `@react-three/postprocessing` to a stable version.
    Removes unnecessary path alias configuration from `vite.config.ts` and `tsconfig.json` as it was not being used and `__dirname` was causing issues.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // FIX: __dirname is not available in all module contexts.
-      // Resolving from the current working directory instead.
-      '@': path.resolve('./src'),
-    },
-  },
 })
